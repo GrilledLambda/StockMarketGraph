da8fa3c4987891bf790f97eb6b39dc9d24009899
Peter <peterjjones98@gmail.com>
2021-05-28 14:28:48 -0700

    Initial Commit

---
+++ StockTest/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+
+
+namespace StockTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<Dictionary<string, string>> stockData = HTTPParser.GetHTML("https://www.marketwatch.com/investing/stock/live");
+            chart1.Titles.Add("Live Stock Report");
+
+            foreach (Dictionary<string, string> stockInfo in stockData)
+            {
+                //add new series to chart and change its chart-type to line
+                chart1.Series.Add(stockInfo["symbol"]);
+                chart1.Series[stockInfo["symbol"]].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+                foreach (KeyValuePair<string, string> stock in stockInfo)
+                {
+                    string currentStock = stock.Key + " | " + stock.Value;
+                    GeneralOutput.Items.Add(currentStock);
+                    chart1.Series[stockInfo["symbol"]].Points.AddXY(0, 0);
+                    chart1.Series[stockInfo["symbol"]].Points.AddXY(1, stockInfo["price"]);
+
+                }
+            }
+            //chart1.Series["Stock 1"].Points.AddXY("1", "10");
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ StockTest/HTTPParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Xml;
+
+
+namespace StockTest
+{
+    public class HTTPParser
+    {
+        public static List<Dictionary<string, string>> GetHTML(string url)
+        {
+            List<Dictionary<string, string>> stockData = new List<Dictionary<string, string>>();
+            HtmlWeb web = new HtmlWeb();
+
+            HtmlDocument document = web.Load(url);
+
+            foreach (HtmlNode row in document.DocumentNode.SelectNodes("//html/body/section/div[2]/div/div[2]/div/div[1]/table//tr"))
+            {
+                Dictionary<string, string> rowDict = new Dictionary<string, string>();
+                foreach (HtmlNode cell in row.SelectNodes("td"))
+                {
+                    string[] cellCls = cell.Attributes["class"].Value.Split(' ');
+                    if (cellCls.Length >= 2)
+                    {
+                        rowDict.Add(cellCls[1], cell.InnerText.Trim() );
+                    }
+                }
+                stockData.Add(rowDict);
+            }
+            return stockData;
+        }
+    }
+}
